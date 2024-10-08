8d21276373aa917b9c2553efd22c78ed17d6ff34
SPIDY123456 <73572033+SPIDY123456@users.noreply.github.com>
2024-10-08 09:58:53 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ActorTable from './components/ActorTable'; 
+import ActorCard from './components/ActorCard.js'; 
+import './index.css';
+
+function App() {
+  return (
+    <div>
+      
+      <ActorTable />
+       <ActorCard /> 
+    </div>
+  );
+}
+
+export default App;
