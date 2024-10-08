c21de3a165f7a0351a3697e3cbcfdf7247b1351a
SPIDY123456 <73572033+SPIDY123456@users.noreply.github.com>
2024-10-08 09:50:46 +0530

    Add files via upload

---
+++ frontend "/src/components/ActorTable.js
@@ -0,0 +1,48 @@
+
+
+import React, { useEffect, useState } from 'react';
+
+const ActorTable = () => {
+    const [actors, setActors] = useState([]);
+
+    
+    useEffect(() => {
+        fetch("http://localhost:7000") 
+            .then(response => response.json())
+            .then(data => setActors(data))
+            .catch(error => console.error('Error fetching actors:', error));
+    }, []);
+
+    return (
+        <div className="p-8 bg-gray-100 min-h-screen">
+            <h1 className="text-4xl font-bold text-center mb-8">Famous Actors List</h1>
+
+            <div className="overflow-x-auto">
+                <table className="min-w-full bg-white shadow-md  text-center rounded-lg">
+                    <thead>
+                        <tr className="bg-yellow-300 text-black text-center">
+                            <th className="py-3 px-4 uppercase font-bold text-lg">Name</th>
+                            <th className="py-3 px-4 uppercase font-bold text-lg">Profession</th>
+                            <th className="py-3 px-4 uppercase font-bold text-lg">Age</th>
+                            <th className="py-3 px-4 uppercase font-bold text-lg">Nationality</th>
+                            <th className="py-3 px-4 uppercase font-bold text-lg">Awards</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {actors.map((actor) => (
+                            <tr key={actor._id} className="hover:bg-gray-200">
+                                <td className="py-5 px-4">{actor.name}</td>
+                                <td className="py-3 px-4">{actor.profession}</td>
+                                <td className="py-3 px-4">{actor.age}</td>
+                                <td className="py-3 px-4">{actor.nationality}</td>
+                                <td className="py-3 px-4">{actor.awards.join(', ')}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+};
+
+export default ActorTable;
